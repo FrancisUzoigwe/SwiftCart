f139a432cd591d349517dd336d70ebf2d789c79c
FrancisUzoigwe <kossyuzoigwe@gmail.com>
2024-01-15 15:51:34 +0100

    small change

--- src/pages/screen/MainScreen.tsx
+++ src/pages/screen/MainScreen.tsx
@@ -3,8 +3,8 @@ import { IoChevronBackCircle } from "react-icons/io5";
 const MainScreen = () => {
   return (
     <div className="w-full h-full flex items-center justify-center">
-      <div className="w-[95%]  h-auto grid gap-2 grid-cols-4 mt-1 max-xl:grid-cols-3 max-md:grid-cols-2">
-        <div className="col-span-3 rounded-lg row-span-2 h-[420px] border flex justify-between items-center relative">
+      <div className="w-[95%] h-auto grid gap-2 grid-cols-4 mt-1 max-xl:grid-cols-3 max-md:grid-cols-2">
+        <div className="xl:col-span-3 max-xl:col-span-2 rounded-lg row-span-2 h-[420px] border flex justify-between items-center relative">
           <div className="w-full h-full absolute flex items-center ">
             <div className="absolute left-2 text-4xl text-white  hover:scale-125 transition-all duration-300 hover:cursor-pointer max-md:text-3xl">
               <IoChevronBackCircle />
